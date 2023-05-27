da4e0d442be01888bbe74330e78435acd8e16502
jacobszpz <jacobszpz@protonmail.com>
2023-05-27 01:10:15 +0100

    Getting closestest 2

--- tests/test_qt.py
+++ tests/test_qt.py
@@ -1,9 +1,13 @@
 import pytest
 from PyQt5.QtWebEngineWidgets import QWebEnginePage
+from PyQt5.QtWidgets import QWidget
 
-class TestSettingsWindow:
+class TestWorkflow:
 
-    def test_settings_window_initial_state(self, qtbot):
-        assert True
+    def test_workflow_error(self, qtbot):
+        widget = QWidget()
+        qtbot.addWidget(widget)
+        widget.show()
+        assert widget.isVisible()
 
 
