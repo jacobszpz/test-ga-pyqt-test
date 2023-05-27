2183adf34c5241a868409731cb6f4d4e24b06d5b
jacobszpz <jacobszpz@protonmail.com>
2023-05-27 16:45:09 +0100

    2

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import time
 import subprocess
 import atexit
 
-from PyQt5.QtWebEngineWidgets import QWebEnginePage
-from PyQt5.QtWidgets import QWidget, QApplication
 
 def test_terminate(proc):
     time.sleep(12)
@@ -14,6 +12,9 @@ def test_terminate(proc):
 
 proc = subprocess.Popen(["Xvfb", ":42"])
 atexit.register(test_terminate, proc)
+
+from PyQt5.QtWebEngineWidgets import QWebEnginePage
+from PyQt5.QtWidgets import QWidget, QApplication
 
 os.environ["DISPLAY"] = ":42"
 time.sleep(1)
